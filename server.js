a8e990b5e808dad6f0155aeb0fc50bef0838cb0a
Dmitriy <dmitriyy311@gmail.com>
2021-06-21 11:42:29 +0300

    editting some files v5

--- server.js
+++ server.js
@@ -11,14 +11,15 @@ require('dotenv').config({path: 'config.env'});
 const app = express();
 const port = process.env.PORT || 5000;
 const DB = process.env.CONNECTION.replace('<PASSWORD>', process.env.PASSWORD);
-app.use(bodyParser.json());
+// app.use(bodyParser.json());
 app.use(cors());
 
 app.use(express.json());
 
-app.use(express.static(path.join(__dirname, '/client/build')))
+// app.use(express.static(path.join(__dirname, '/client/build')))
+app.use(express.static(__dirname))
 
-app.get('*', (req, res) => {
+app.get('/*', (req, res) => {
     res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
 });
 
